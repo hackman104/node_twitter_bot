619de19da6d67ba9678545d679e654e8009b3359
hackman104 <stimsond@umich.edu>
2018-05-23 11:09:11 -0400

    added response to new followers

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-This is as basic twitter bot that will allow you to retweet items basic on a search string. It is based on the walkthrough found here:
+This is as basic twitter bot that will allow you to retweet items basic on a search string. It will also respond to users who follow it with a short message. It is based on the walkthroughs found here:
 https://hackernoon.com/create-a-simple-twitter-bot-with-node-js-5b14eb006c08
+https://community.risingstack.com/how-to-make-a-twitter-bot-with-node-js/
 
 I use a dotenv file to avoid the security risk of displaying my Twitter app credentials online. Please create a .env file in your project's home directory and populate it with your credentials.
--- bot.js
+++ bot.js
@@ -7,6 +7,46 @@ var config = require('./config.js');
 // declare our Twitter handler
 var Twitter = new twit(config);
 
+// set up stream to interact with users
+var stream = Twitter.stream('user');
+
+// when someone follows, call the followed function
+stream.on('follow', followed)
+
+// get user info, trigger callback
+function followed(event) {
+    console.log('New follower detected');
+
+    // get user's twitter info
+    var name = event.source.name,
+    screenName = event.source.screen_name;
+
+    // call function to reply to new follower
+    tweetNow('@' + screenName + ' Thank you for following!');
+}
+
+// function that updates status based on user following
+function tweetNow(tweetTxt) {
+    
+    // set up tweet
+    var tweet = {
+        status: tweetTxt
+    }
+
+    // post tweet
+    Twitter.post('statuses/update', tweet, function(err, data, response) {
+        
+        // notify whether error occurred or successfully replied
+        if (err) {
+            console.log("Error replying to new follower.");
+        }
+        else {
+            console.log("Successfully tweeted to new follower.");
+        }
+    });
+
+}
+
 // retweet function
 var retweet = function() {
 
